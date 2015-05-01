d835f182fd98632a8b66efa71a9c6cec3a4e2403
Kenneth Hoste <kenneth.hoste@ugent.be>
2015-05-01 14:57:27 +0200

    use curl -L, update /Users/kehoste/.local/pylint/bin:/Users/kehoste/work/easybuild-framework:/Users/kehoste/lmod/5.9/libexec:/Users/kehoste/.local/environment-modules/Modules/3.2.10/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/usr/local/MacGPG2/bin:/usr/texbin when installing EB

--- install_EasyBuild.sh
+++ install_EasyBuild.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-curl -O https://raw.githubusercontent.com/hpcugent/easybuild-framework/develop/easybuild/scripts/bootstrap_eb.py
+# lmod binary must be available to EasyBuild
+export PATH=$HOME/lmod/lmod/libexec:$PATH
+curl -L -O https://raw.githubusercontent.com/hpcugent/easybuild-framework/develop/easybuild/scripts/bootstrap_eb.py
 python bootstrap_eb.py $HOME
--- install_Lmod.sh
+++ install_Lmod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 LMOD_VERSION=5.9.4
-curl -O https://github.com/TACC/Lmod/archive/${LMOD_VERSION}.tar.gz
+curl -L -O https://github.com/TACC/Lmod/archive/${LMOD_VERSION}.tar.gz
 tar xvfz ${LMOD_VERSION}.tar.gz
 cd Lmod-${LMOD_VERSION}
 # will install to $HOME/lmod
