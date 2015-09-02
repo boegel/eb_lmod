17602afd119894ac47f4d90ca0bae3baa42776a3
Kenneth Hoste <kenneth.hoste@ugent.be>
2015-09-02 22:00:20 +0200

    add path to modulecmd to $PATH, only use Lmod if it's available

--- setup_bashrc.sh
+++ setup_bashrc.sh
@@ -3,8 +3,8 @@
 # disable coloured ls output
 alias ls="/bin/ls"
 
-# include location of Lmod binaries in \$PATH
-export PATH=\$HOME/lmod/lmod/libexec:\$PATH
+# include location of environment modules/Lmod binaries in \$PATH
+export PATH=`ls -d \$HOME/env-mods/Modules/*/bin`:\$HOME/lmod/lmod/libexec:\$PATH
 
 # set up \$MODULEPATH (can't use 'module use' yet)
 export MODULEPATH=\$HOME/modules/all
@@ -13,11 +13,17 @@ export MODULEPATH=\$HOME/modules/all
 source \$HOME/lmod/lmod/init/bash
 
 ## configure EasyBuild
+
 # install software to \$HOME/software, modules to \$HOME/modules(/all)
 export EASYBUILD_PREFIX=\$HOME
-# use Lmod as modules tool
-export EASYBUILD_MODULES_TOOL=Lmod
+
+# use Lmod as modules tool (if available)
+which lmod > /dev/null
+if [ $? -eq 0 ]
+then
+    export EASYBUILD_MODULES_TOOL=Lmod
+fi
 
 # load EasyBuild module
-ml EasyBuild
+module load EasyBuild
 EOF
