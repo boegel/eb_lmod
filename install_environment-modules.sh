b3f0b5938af57e2c5c03124dcaf804421412efe9
Kenneth Hoste <kenneth.hoste@ugent.be>
2015-09-02 21:53:09 +0200

    fix --prefix

--- install_environment-modules.sh
+++ install_environment-modules.sh
@@ -3,5 +3,5 @@ ENV_MOD_VERSION=3.2.10
 curl -L -O http://downloads.sourceforge.net/project/modules/Modules/modules-${ENV_MOD_VERSION}/modules-${ENV_MOD_VERSION}.tar.gz
 tar xfvz modules-${ENV_MOD_VERSION}.tar.gz
 cd modules-${ENV_MOD_VERSION}
-./configure --prefix=env-mods
+./configure --prefix=$HOME/env-mods
 make install
